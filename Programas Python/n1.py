61a594aaed63c7d92eebcda225b95c83c9226784
DoraVentureiro <36071045+DoraVentureiro@users.noreply.github.com>
2018-02-02 08:47:45 -0200

    Create n1.py

---
+++ Programas Python/n1.py
@@ -0,0 +1,8 @@
+nt1 = int(input("Primeira nota? "))
+nt2 = int(input("Segunda nota? "))
+nt3 = int(input("Terceira nota? "))
+nt4 = int(input("Quarta nota? "))
+
+mdia = (nt1 + nt2 + nt3 + nt4)/4
+
+print("Sua média é:",mdia)
